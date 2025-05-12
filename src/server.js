2f72b81a22a1c1395c58706cd505fa8635c8ed4d
HuuThang <huuthangblue0512@gmail.com>
2025-05-12 14:17:59 +0700

    Init code base

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
 import express from "express"
+import { mapOrder } from "~/utils/sorts.js"
 
 const app = express()
 
-const host = "localhost"
+const hostname = "localhost"
 const port = 8017
+
 app.get("/", (req, res) => {
-  res.send("<h2>Hello World NodeJS</h2>")
+  // Test Absolute import mapOrder
+  console.log(
+    mapOrder(
+      [
+        { id: "id-1", name: "One" },
+        { id: "id-2", name: "Two" },
+        { id: "id-3", name: "Three" },
+        { id: "id-4", name: "Four" },
+        { id: "id-5", name: "Five" }
+      ],
+      ["id-5", "id-4", "id-2", "id-3", "id-1"],
+      "id"
+    )
+  )
+  res.end("<h1>Hello World!</h1><hr>")
 })
 
-app.listen(port, host, () => {
-  console.log("Hi, Im running server on port", port)
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Huu Thang, I am running at http://${hostname}:${port}/`)
 })
