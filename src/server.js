daefddeb28fe94eb9959aa422ddb2ec069865a4e
HuuThang <huuthangblue0512@gmail.com>
2025-05-24 20:43:40 +0700

    middleware_error_handle

--- src/server.js
+++ src/server.js
@@ -3,18 +3,22 @@ import exitHook from "async-exit-hook"
 import { CONNECT_DB, GET_DB, CLOSE_DB } from "./config/mongodb.js"
 import { env } from "~/config/environment"
 import { APIs_V1 } from "~/routes/v1"
+import { errorHandlingMiddleware } from "~/middlewares/errorHandlingMiddleware.js"
 const START_SERVER = () => {
   const app = express()
 
   app.use(express.json())
-
+  
   app.use("/v1", APIs_V1)
-  app.get("/", async (req, res) => {
-    // Test Absolute import mapOrder
-    console.log(await GET_DB().listCollections().toArray())
-    res.end("<h1>Hello World!</h1><hr>")
-  })
-
+  // app.get("/", async (req, res) => {
+    //   // Test Absolute import mapOrder
+    //   console.log(await GET_DB().listCollections().toArray())
+    //   res.end("<h1>Hello World!</h1><hr>")
+    // })
+    
+    
+    // Middleware xử lý lỗi tập trung
+    app.use(errorHandlingMiddleware)
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
     console.log(
