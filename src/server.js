888f2c3c7f5ad3520542d6cc495844c1f4d4b10c
HuuThang <huuthangblue0512@gmail.com>
2025-05-25 22:27:58 +0700

    mode_dev_production_handle_error

--- src/server.js
+++ src/server.js
@@ -8,17 +8,16 @@ const START_SERVER = () => {
   const app = express()
 
   app.use(express.json())
-  
+
   app.use("/v1", APIs_V1)
   // app.get("/", async (req, res) => {
-    //   // Test Absolute import mapOrder
-    //   console.log(await GET_DB().listCollections().toArray())
-    //   res.end("<h1>Hello World!</h1><hr>")
-    // })
-    
-    
-    // Middleware xử lý lỗi tập trung
-    app.use(errorHandlingMiddleware)
+  //   // Test Absolute import mapOrder
+  //   console.log(await GET_DB().listCollections().toArray())
+  //   res.end("<h1>Hello World!</h1><hr>")
+  // })
+
+  // Middleware xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware)
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     // eslint-disable-next-line no-console
     console.log(
